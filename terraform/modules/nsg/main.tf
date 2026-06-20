c8c5371158e3dabe22ea744014ddb7981da4138d
Ubuntu <azureuser@vm-devops-lab.icmej2pwwuaebob41n0bomxpnb.rx.internal.cloudapp.net>
2026-06-20 17:07:51 +0000

    Fix terraform formatting

--- terraform/modules/nsg/main.tf
+++ terraform/modules/nsg/main.tf
@@ -7,16 +7,16 @@ resource "azurerm_network_security_group" "this" {
 resource "azurerm_network_security_rule" "rules" {
   for_each = { for r in var.rules : r.name => r }
 
-  name                        = each.value.name
-  priority                    = 100 + each.value.priority_offset
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
+  name      = each.value.name
+  priority  = 100 + each.value.priority_offset
+  direction = "Inbound"
+  access    = "Allow"
+  protocol  = "Tcp"
 
   source_port_range          = "*"
   destination_port_range     = each.value.port
   source_address_prefix      = "*"
-  destination_address_prefix  = "*"
+  destination_address_prefix = "*"
 
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.this.name
